de23c354dd2e4d5d04e791ff678c7551d1a1e075
PFCCDFF <parisfccdff@gmail.com>
2025-11-01 12:35:47 +0100

    Update paris_football_club.py

--- paris_football_club.py
+++ paris_football_club.py
@@ -631,7 +631,7 @@ def create_individual_radar(df):
             kwargs_values=dict(
                 color='#FFFFFF',
                 fontsize=3,
-                bbox=dict(edgecolor='#FFFFFF', facecolor='#0e1117', boxstyle='round, pad=0.1', lw=1)
+                bbox=dict(edgecolor='#FFFFFF', facecolor='#0e1117', boxstyle='round, pad=0.3', lw=1)
             ),
             kwargs_params=dict(color='#FFFFFF', fontsize=3, fontproperties='monospace')
         )
@@ -1332,6 +1332,7 @@ if __name__ == '__main__':
 
     # Appel de la fonction principale de l'interface
     script_streamlit(pfc_kpi, edf_kpi, permissions, st.session_state.user_profile)
+
 
 
 
